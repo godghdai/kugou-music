d58ab1d1b009f99362600c2651e480527363f8ad
godghdai <godghdai@gmail.com>
2017-11-05 05:32:04 +0800

    player

--- src/components/player/index.vue
+++ src/components/player/index.vue
@@ -1,27 +1,48 @@
 <template>
+  <div>
+    <div class="minplayer">
+      <img class="photo" :src="currentSong.imgUrl" alt=""/>
+      <div class="info">
+        <p class="song">{{currentSong.songName}}</p>
+        <p class="singer">{{currentSong.singerName}}</p>
+      </div>
+      <div class="icos">
+        <i :class="status" @click="toggle"></i>
+        <i class="ico_prev" @click="playPrev"></i>
+        <i class="ico_next" @click="playNext"></i>
+      </div>
+    </div>
     <audio ref="audio" :src="currentSong.playurl" @play="ready" @error="error" @timeupdate="updateTime"
            @ended="end" @pause="paused"></audio>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters} from 'vuex';
+  import {mapGetters, mapActions} from 'vuex';
   export default {
     mixins: [],
     data() {
       return {
-
+        songReady: false,
+        playing: false
       }
     },
     computed: {
       ...mapGetters([
         'currentSong'
-      ])
+      ]),
+      status() {
+        return this.songReady ? "ico_pause" : "ico_loading";
+      }
     },
     created() {
 
     },
     methods: {
+      ...mapActions([
+       'playPrev',
+       'playNext'
+      ]),
       ready() {
         this.songReady = true;
       },
@@ -35,6 +56,9 @@
 
       },
       paused() {
+
+      },
+      toggle() {
 
       }
     },
@@ -55,5 +79,69 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+  @import "~common/stylus/variable";
+  .minplayer
+    position:fixed
+    height:152pxx
+    width:100%
+    left: 0;
+    bottom: 0
+    background: rgba(0,0,0,.9)
+    color: #fff
+    z-index:999
+    .photo
+      margin-top:8pxx
+      margin-left:8pxx
+      width:136pxx
+      height:136pxx
+      border-radius: 15pxx
+      display: inline-block
+      vertical-align: middle
+    .info
+      width: 65%
+      display: inline-block
+      vertical-align: middle
+      .song
+        width: 100%
+        line-height: 1.7
+        overflow: hidden
+        white-space: nowrap
+        text-overflow: ellipsis
+        font-size: 28pxx
+        color: #fff
+      .singer
+        line-height: 1.3
+        font-size: 24pxx
+        color: #888
+    .icos
+      position: absolute
+      top: 40pxx
+      right: 20pxx
+      .ico_play,.ico_prev,.ico_next,.ico_loading,.ico_pause
+        width: 70pxx
+        height: 70pxx
+        display: inline-block
+        vertical-align: middle
+        cursor: pointer
+        background-size: 100%
+      .ico_loading
+        background: url(./loading.png) no-repeat center
+        transform-origin: center
+        animation:rotate 1s linear infinite
+      .ico_pause
+        background: url(./pause_icon.png) no-repeat center
+      .ico_play
+        background: url(./play_icon.png) no-repeat center
+      .ico_prev
+        background: url(./prev_icon.png) no-repeat center
+      .ico_next
+        background: url(./next_icon.png) no-repeat center
+
+  @keyframes rotate
+    0%
+      transform:rotate(0deg)
+    50%
+      transform:rotate(180deg)
+    100%
+      transform:rotate(360deg)
 </style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,31 @@
 import * as types from './mutation-types';
 import { getSongInfoJson } from "api";
-export const addSongToPlayList = function ({ commit, state }, song) {
-     let playlist = state.playlist.slice();
-     getSongInfoJson(song).then(info => {
-      "http://imge.kugou.com/stdmusic/100/20150717/20150717160416784222.jpg"
-         song["album_img"] = info.album_img;
-         song["fileName"] = info.fileName;
-         song["imgUrl"] = info.imgUrl;
-         song["playurl"] = info.url;
-         playlist.push(song);
-         commit(types.SET_PLAYLIST, playlist);
-         commit(types.SET_CURRENT_INDEX, playlist.length - 1);
-         console.log(song);
-     });
+export const addSongToPlayList = function({ commit, state }, song) {
+  let playlist = state.playlist.slice();
+  getSongInfoJson(song).then(info => {
+    song["album_img"] = info.album_img;
+    song["fileName"] = info.fileName;
+    song["imgUrl"] = info.imgUrl.replace("{size}", 100);
+    song["playurl"] = info.url;
+    song["songName"] = info.songName;
+    song["singerName"] = info.singerName;
+    playlist.push(song);
+    commit(types.SET_PLAYLIST, playlist);
+    commit(types.SET_CURRENT_INDEX, playlist.length - 1);
+    console.log(song);
+  });
+}
+
+export const playPrev = function({ commit, state }) {
+  let prev = state.currentIndex - 1;
+  let playlist = state.playlist;
+  if (prev < 0) prev = playlist.length - 1;
+  commit(types.SET_CURRENT_INDEX, prev);
+}
+
+export const playNext = function({ commit, state }) {
+  let next = state.currentIndex + 1;
+  let playlist = state.playlist;
+  if (next > playlist.length) next = 0;
+  commit(types.SET_CURRENT_INDEX, next);
 }
